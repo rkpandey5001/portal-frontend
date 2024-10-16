55399910ea2c003903f31f2194e4a9e85fcdaa4c
RAHUL KUMAR PANDEY <rkpandey5001@gmail.com>
2024-10-16 22:22:32 +0530

    seventh-commit

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,20 +1,20 @@
 // export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
 export const USER_API_END_POINT =
-  "http://ec2-65-1-95-253.ap-south-1.compute.amazonaws.com/api/v1/user";
+  "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/user";
 // export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
 
 export const JOB_API_END_POINT =
-  "http://ec2-65-1-95-253.ap-south-1.compute.amazonaws.com/api/v1/job";
+  "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/job";
 // export const Application_API_END_POINT =
 //   "http://localhost:3000/api/v1/applicant";
 
 export const Application_API_END_POINT =
-  "http://ec2-65-1-95-253.ap-south-1.compute.amazonaws.com/api/v1/applicant";
+  "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/applicant";
 // export const Company_API_END_POINT = "http://localhost:3000/api/v1/company";
 
 export const Company_API_END_POINT =
-  "http://ec2-65-1-95-253.ap-south-1.compute.amazonaws.com/api/v1/company";
+  "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/company";
 // export const Admin_Job_API_END_POINT = "http://localhost:3000/api/v1/job";
 
 export const Admin_Job_API_END_POINT =
-  "http://ec2-65-1-95-253.ap-south-1.compute.amazonaws.com/api/v1/job";
+  "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/job";
