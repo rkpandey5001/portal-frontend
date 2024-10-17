6b9e556eb04a072f212c99d25ddc6c261b2f533b
RAHUL KUMAR PANDEY <rkpandey5001@gmail.com>
2024-10-17 20:27:36 +0530

    update-host

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,25 +1,25 @@
-// export const USER_API_END_POINT =
-//   "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/user";
+export const USER_API_END_POINT =
+  "http://ec2-3-110-221-248.ap-south-1.compute.amazonaws.com/api/v1/user";
 
-// export const JOB_API_END_POINT =
-//   "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/job";
-
-// export const Application_API_END_POINT =
-//   "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/applicant";
-
-// export const Company_API_END_POINT =
-//   "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/company";
-
-// export const Admin_Job_API_END_POINT =
-//   "http://ec2-3-109-108-236.ap-south-1.compute.amazonaws.com/api/v1/job";
-
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
-
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
+export const JOB_API_END_POINT =
+  "http://ec2-3-110-221-248.ap-south-1.compute.amazonaws.com/api/v1/job";
 
 export const Application_API_END_POINT =
-  "http://localhost:3000/api/v1/applicant";
+  "http://ec2-3-110-221-248.ap-south-1.compute.amazonaws.com/api/v1/applicant";
 
-export const Company_API_END_POINT = "http://localhost:3000/api/v1/company";
+export const Company_API_END_POINT =
+  "http://ec2-3-110-221-248.ap-south-1.compute.amazonaws.com/api/v1/company";
 
-export const Admin_Job_API_END_POINT = "http://localhost:3000/api/v1/job";
+export const Admin_Job_API_END_POINT =
+  "http://ec2-3-110-221-248.ap-south-1.compute.amazonaws.com/api/v1/job";
+
+// export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
+
+// export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
+
+// export const Application_API_END_POINT =
+//   "http://localhost:3000/api/v1/applicant";
+
+// export const Company_API_END_POINT = "http://localhost:3000/api/v1/company";
+
+// export const Admin_Job_API_END_POINT = "http://localhost:3000/api/v1/job";
